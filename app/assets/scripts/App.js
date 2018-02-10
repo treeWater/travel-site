8ad0253fe4e9b13fb84336a9ce3f3a03ee75b9bc
Thomas Chang <tianlong8bu@gmail.com>
2018-02-10 15:06:13 +0800

    sticky header done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "78%");
 new RevealOnScroll($(".testimonial"), "70%");
+var stickyHeader = new StickyHeader();
